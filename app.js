e415a49a4d9b4dd46a9e149d3b06b61e416b54f0
Erki <erkiparna7@gmail.com>
2022-10-20 02:56:45 +0300

    removeBook() removeBookStorage()

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const table = document.querySelector('#tabel')
 const body = table.querySelector('tbody')
 
 form.addEventListener('submit', addBook)
+body.addEventListener('click', removeBook)
 
 function addBook(event) {
     const Row = `<tr>
@@ -30,5 +31,33 @@ function addBookStorage(bookName, bookAuthor, bookISBN) {
         books = JSON.parse(localStorage.getItem('books'))
     }
     books.push([bookName, bookAuthor, bookISBN])
+    localStorage.setItem('books', JSON.stringify(books))
+}
+
+function removeBook(event) {
+    if(event.target.textContent == 'X') {
+        let bookISBN = event.target.parentElement.previousElementSibling
+        let bookAuthor = bookISBN.previousElementSibling
+        let bookName = bookAuthor.previousElementSibling
+
+        removeBookStorage(bookName.textContent, bookAuthor.textContent, bookISBN.textContent)
+        event.target.parentElement.parentElement.remove()
+    }
+}
+
+function removeBookStorage(bookName, bookAuthor, bookISBN) {
+    let books
+    if(localStorage.getItem('books') === null) {
+        books = []
+    } else {
+        books = JSON.parse(localStorage.getItem('books'))
+    }
+    books.forEach((book, bookIndex) => {
+        if(book[0] == bookName && book[1] == bookAuthor && book[2] == bookISBN) {
+            console.log('found book')
+            books.splice(bookIndex, 1)
+        }
+    })
+
     localStorage.setItem('books', JSON.stringify(books))
 }
