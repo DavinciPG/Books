641d97c242c7c51e01713b785fcab00e6a321b52
Erki <erkiparna7@gmail.com>
2022-10-20 10:54:45 +0300

    Book conversion to OOP [Delivery #183586108]

--- app.js
+++ app.js
@@ -1,47 +1,27 @@
 const form = document.querySelector('form')
 
-const title = document.querySelector('#title')
-const author = document.querySelector('#author')
-const isbn = document.querySelector('#isbn')
-
-const table = document.querySelector('#tabel')
-const body = table.querySelector('tbody')
+const ui = new UI();
+const ls = new LS();
 
 form.addEventListener('submit', addBook)
-body.addEventListener('click', removeBook)
-document.addEventListener('DOMContentLoaded', getBookFromLocalStorage)
-
-class Book {
-    constructor(nimi, isbn, author) {
-        this.name = nimi;
-        this.isbn = isbn;
-        this.author = author;
-    }
-}
+table.addEventListener('click', removeBook)
+document.addEventListener('DOMContentLoaded', getBooks)
 
 function addBook(event) {
+    const title = document.querySelector('#title')
+    if(title.value == '')
+        return
+    const author = document.querySelector('#author')
+    if(author.value == '')
+        return
+    const isbn = document.querySelector('#isbn')
+    if(isbn.value == '')
+        return
     const book = new Book(title.value, isbn.value, author.value)
-    const Row = `<tr>
-                   <td id="tiitel">${book.name}</td>
-                   <td id="autor">${book.author}</td>
-                   <td id="isbnkood">${book.isbn}</td>
-                   <td><a href="#">X</a></td>
-                </tr>`
-
-    body.insertAdjacentHTML('beforeend', Row)
-    addBookStorage(book)
-    event.preventDefault();
-}
-
-function addBookStorage(book) {
-    let books
-    if(localStorage.getItem('books') === null) {
-        books = []
-    } else {
-        books = JSON.parse(localStorage.getItem('books'))
-    }
-    books.push(book)
-    localStorage.setItem('books', JSON.stringify(books))
+    console.log('APP addBook: ', book)
+    ui.addBook(book)
+    ls.addBook(book)
+    ui.clearFields()
 }
 
 function removeBook(event) {
@@ -51,43 +31,13 @@ function removeBook(event) {
         let bookName = bookISBN.previousElementSibling
 
         const book = new Book(bookName.textContent, bookISBN.textContent, bookAuthor.textContent)
-        removeBookStorage(book)
-        event.target.parentElement.parentElement.remove()
+        ls.removeBook(book)
+        ui.removeBook(event)
     }
 }
 
-function removeBookStorage(ibook) {
-    let books
-    if(localStorage.getItem('books') === null) {
-        books = []
-    } else {
-        books = JSON.parse(localStorage.getItem('books'))
-    }
-    books.forEach((book, bookIndex) => {
-        if(book.name == ibook.name && book.isbn == ibook.isbn && book.author == ibook.author) {
-            books.splice(bookIndex, 1)
-        }
-    })
-
-    localStorage.setItem('books', JSON.stringify(books))
-}
-
-function getBookFromLocalStorage() {
-        let books
-    if(localStorage.getItem('books') === null) {
-        books = []
-    } else {
-        books = JSON.parse(localStorage.getItem('books'))
-    }
-
-    books.forEach((book) => {
-        const Row = `<tr>
-                   <td>${book.name}</td>
-                   <td>${book.isbn}</td>
-                   <td>${book.author}</td>
-                   <td><a href="#">X</a></td>
-                </tr>`
-
-        body.insertAdjacentHTML('beforeend', Row)
-    })
+function getBooks() {
+    let books = ls.getData('books')
+    console.log('Book data: ', books)
+    ui.getBooks(books)
 }
---
+++ book.js
@@ -0,0 +1,7 @@
+class Book {
+    constructor(nimi, isbn, author) {
+        this.name = nimi;
+        this.isbn = isbn;
+        this.author = author;
+    }
+}
